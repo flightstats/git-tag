f5b6027d87d3d0d799d52c5b97021274979ee748
Thomas Burke <etburke4@gmail.com>
2015-02-05 10:00:55 -0800

    optional semver, added diff methods

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var semver = require('semver')
 module.exports = function(options) {
   options = options || {}
 
+  if (!options.hasOwnProperty('semverSort')) {
+    options.semverSort = true;
+  }
+
   var get = function(cb) {
     var cmd = 'git tag -l'
     if (!options.localOnly) {
@@ -15,9 +19,17 @@ module.exports = function(options) {
     }
     exec(cmd, function(err, res){
       if (err) console.warn('WARN: ' + err)
-      if (err || !res.length) return cb([])
-      res = res.replace(/^\s+|\s+$/g,'').split(/\n/).sort(semver.compare)
-      cb(err ? [] : res)
+      if (err || !res.length) return cb(err, [])
+      res = res.replace(/^\s+|\s+$/g,'').split(/\n/)
+      
+      if (options.semverSort) {
+        res = res.sort(semver.compare)
+      }
+      else {
+        res = res.sort()
+      }
+
+      cb(err, err ? [] : res)
     })
   }
 
@@ -29,7 +41,7 @@ module.exports = function(options) {
     }
     exec(cmd, function(err){
       if (err) console.warn('WARN: ' + err)
-      cb(name)
+      cb(err, name)
     })
   }
 
@@ -40,17 +52,39 @@ module.exports = function(options) {
     }
     exec(cmd, function(err){
       if (err) console.warn('WARN: ' + err)
-      cb(name)
+      cb(err, name)
     })
+  }
+
+  var diff = function(name, cb) {
+    var cmd = 'git diff ' + name
+    exec(cmd, function(err, res) {
+      if (err) console.warn('WARN: ' + err)
+      console.log("diffs:", res);
+      cb(err, res);
+    })
+  }
+
+  var changedSince = function(name, cb) {
+    diff(name, function(err, res) {
+      if (err) {
+        cb(err, null);
+      }
+      else {
+        cb(null, !!res);
+      }
+    });
   }
 
   var Tag = {
     create: create,
     remove: remove,
+    diff: diff,
+    changedSince: changedSince,
     all: get,
     latest: function(cb) {
-      get(function(res){
-        cb(res.pop())
+      get(function(err, res){
+        cb(err, res.pop())
       })
     }
   }
